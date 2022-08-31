6afdb0c59ac640b83e6f0b4443109eaccbaeee4c
Pinogpog <102848360+Pinogpog@users.noreply.github.com>
2022-08-31 21:40:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from os import getenv
 
 )
-BOT_TOKEN = getenv("BOT_TOKEN", "Jamjamxmusicbot")
+BOT_TOKEN = getenv("BOT_TOKEN", "5621197727:AAGUdK7AErXR-j2UC-p_8RIa_euprsSNf2U")
 API_ID = int(getenv("API_ID", "9688162"))
 API_HASH = getenv("API_HASH", "7b2bd6db5aaf2fc6c21d08249189c028")
 OWNER_ID = getenv("OWNER_ID", "1620497594")
